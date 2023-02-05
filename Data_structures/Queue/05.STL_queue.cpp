8e9b77e7548eb247d64583efb8e7be1441f7c97a
tamim0133 <tamimdewan2003@gmail.com>
2023-02-05 22:48:36 +0600

    Que added

---
+++ Data_structures/Queue/05.STL_queue.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+    // enqueue O(1)
+    q.push(5);
+    q.push(10);
+    q.push(15);
+
+    // size O(1)
+    cout << "Queue size : " << q.size() << "\n";
+    // empty O(1)
+    cout << q.empty() << "\n";
+
+    // front O(1)
+    cout << q.front() << "\n";
+
+    // dequeue O(1)
+    q.pop();
+
+    cout << q.front() << "\n";
+    q.pop();
+
+    cout << q.front() << "\n";
+    q.pop();
+
+    cout << q.empty() << "\n";
+
+    queue<char> q2;
+    q2.push('a');
+    q2.push('b');
+    q2.push('c');
+
+    cout << q2.front() << "\n";
+
+    q2.pop();
+
+    cout << q2.front() << "\n";
+    q2.pop();
+
+    cout << q2.front() << "\n";
+    q2.pop();
+    return 0;
+}
+/*
+The queue gquiz is :     10    20    30
+
+gquiz.size() : 3
+gquiz.front() : 10
+gquiz.back() : 30
+gquiz.pop() :     20    30
+*/
