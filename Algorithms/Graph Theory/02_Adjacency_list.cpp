efbe147201cc8100eb4a99f2d83d4e9ea684ebc9
tamim0133 <tamimdewan2003@gmail.com>
2023-03-24 00:22:45 +0600

    Quesiotn add kore dichi

--- Algorithms/Graph Theory/02_Adjacency_list.cpp
+++ Algorithms/Graph Theory/02_Adjacency_list.cpp
@@ -1,41 +1,30 @@
-// A simple representation of graph using STL
 #include <bits/stdc++.h>
+
 using namespace std;
 
-// A utility function to add an edge in an
-// undirected graph.
-void addEdge(vector<int> adj[], int u, int v)
-{
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-}
-
-// A utility function to print the adjacency list
-// representation of graph
-void printGraph(vector<int> adj[], int V)
-{
-    for (int v = 0; v < V; ++v)
-    {
-        cout << "\n Adjacency list of vertex " << v
-             << "\n head ";
-        for (auto x : adj[v])
-            cout << "-> " << x;
-        cout << endl;
-    }
-}
-
-// Driver code
 int main()
 {
-    int V = 5;
-    vector<int> adj[V];
-    addEdge(adj, 0, 1);
-    addEdge(adj, 0, 4);
-    addEdge(adj, 1, 2);
-    addEdge(adj, 1, 3);
-    addEdge(adj, 1, 4);
-    addEdge(adj, 2, 3);
-    addEdge(adj, 3, 4);
-    printGraph(adj, V);
+    int n = 9;
+    vector<int> adjList[n + 1];
+
+    int e;
+    cin >> e;
+
+    for (int i = 1; i <= e; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " -> ";
+        for (auto child : adjList[i])
+            cout << child << " ";
+        cout << endl;
+    }
+
     return 0;
 }
